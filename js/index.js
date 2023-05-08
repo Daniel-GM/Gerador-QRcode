4c90b1c0df8348d459bb93d181dc224b0c6ab526
Daniel-GM <danielgomesmoura@gmail.com>
2023-05-08 15:13:51 -0300

    async no gerador das imagens

--- js/index.js
+++ js/index.js
@@ -1,46 +1,39 @@
-function gerarComanda() {
+async function gerarComanda() {
   const formLogo = document.getElementById('form-logo')
   const formPhoto = document.getElementById('form-photo')
   const result = document.getElementById('result')
   const cor = document.getElementById('cor')
   result.textContent = ''
   
-  setTimeout(() => {
-    for (let index = 0; index < formPhoto.files.length; index++) {
-      const photo = formPhoto.files[index]
-      /* criando a logo */
-      const logo = document.createElement('img')
-      logo.src = URL.createObjectURL(formLogo.files[0])
-      logo.style.height = "200px"
-      logo.style.width = "280px"
+  
+  for (let index = 0; index < formPhoto.files.length; index++) {
+    const photo = formPhoto.files[index]
+    /* criando a logo */
+    const logo = document.createElement('img')
+    logo.src = URL.createObjectURL(formLogo.files[0])
+    logo.style.height = "200px"
+    logo.style.width = "280px"
 
-      /* colocando o QRcode */
-      const img = document.createElement('img')
-      img.src = URL.createObjectURL(photo)
-      img.style.height = "357px"
-      img.style.width = "280px"
+    /* colocando o QRcode */
+    const img = document.createElement('img')
+    img.src = URL.createObjectURL(photo)
+    img.style.height = "357px"
+    img.style.width = "280px"
 
-      /* criando comanda */
-      const comanda = document.createElement('div')
-      comanda.classList.add('comanda')
-      comanda.style.height = "1854"
-      comanda.style.width = "991"
-      comanda.style.backgroundColor = cor.value
+    /* criando comanda */
+    const comanda = document.createElement('div')
+    comanda.classList.add('comanda')
+    comanda.style.height = "1854"
+    comanda.style.width = "991"
+    comanda.style.backgroundColor = cor.value
 
-      comanda.appendChild(logo)
-      comanda.appendChild(img)
-      result.appendChild(comanda)
-    }
-  }, 1000)
+    comanda.appendChild(logo)
+    comanda.appendChild(img)
+    result.appendChild(comanda)
 
-  debugger
-  const imagem = result.children
-  for (let index = 0; index < imagem.length; index++) {
-    const div = imagem[index]
-    html2canvas(div).then(function(canvas) {
-      result.appendChild(canvas)
-    });
-    div.style.display = 'none'
+    const canvas = await html2canvas(comanda)
+    comanda.style.display = 'none'
+    result.appendChild(canvas)
   }
   const download = document.getElementById('download-form')
   download.style.display = 'inline'
