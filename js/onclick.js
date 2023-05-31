f4abbd75d587fb19df394d1ed8e93c91909afe00
Daniel-GM <danielgomesmoura@gmail.com>
2023-05-31 09:11:30 -0300

    qualidade de vida

--- js/onclick.js
+++ js/onclick.js
@@ -2,6 +2,11 @@ function inputName() {
   let cardapioChecked = document.querySelector('#cardapio')
   let option = document.getElementsByName('option')
   let dominio = document.getElementById('div-dominio')
+  let logoSigesis = document.getElementById('div-sigesis')
+  
+  debugger
+  if(cardapioChecked.checked) logoSigesis.style.display = 'flex'
+  else logoSigesis.style.display = 'none'
   
   if(option[1].checked && cardapioChecked.checked) {
     dominio.style.display = 'flex'
